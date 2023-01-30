fb36c6c22c986086c11626f742c7c3196f25f52a
tami <xorud7979@gmail.com>
2023-01-30 17:52:21 +0900

    js prac

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const h1 = document.getElementById("title");
+// =============================================================================
+// javascript로 css 스타일 변경
+// =============================================================================
+// const h1 = document.getElementById("title");
 
 // =============================================================================
 // className로 스타일 변경 하는 방법
 // =============================================================================
-function handleTitleClick() {
-  h1.classList.toggle("clicked");
-}
-h1.addEventListener("click", handleTitleClick);
+// function handleTitleClick() {
+//   h1.classList.toggle("clicked");
+// }
+// h1.addEventListener("click", handleTitleClick);
 
 // =============================================================================
 // javascript로 스타일 변경 하는 방법
@@ -26,16 +29,66 @@ h1.addEventListener("click", handleTitleClick);
   h1.style.color = newColor;
 }
 h1.addEventListener("click", handleTitleClick);*/
-const bodyColor = document.querySelector("body");
 
-function handleWindowSize () {
-  const WindowSize = window.innerWidth;
-  if(WindowSize >= 700){
-    bodyColor.style.backgroundColor = "blue"
-  }else if(WindowSize < 700 && WindowSize > 400) {
-    bodyColor.style.backgroundColor = "orange"
-  }else {
-    bodyColor.style.backgroundColor = "yellow"
+// =============================================================================
+// javascript-input
+// =============================================================================
+// const loginForm = document.querySelector("#login-form");
+// const loginInput = document.querySelector("#login-form input");
+// const greeting = document.querySelector("#greeting");
+
+// const HIDDEN_CLASSNAME = "hidden";
+// const USERNAME_KEY = "username";
+
+// function onLoginSubmit(event) {
+//   //step1-화면 새로고침 방지
+//   event.preventDefault();
+//   //step2-form을 hidden 시킴
+//   loginForm.classList.add(HIDDEN_CLASSNAME);
+//   const username = loginInput.value
+//   //step3-storage에 key는 'username'으로 저장, name은 input.value로 저장
+//   localStorage.setItem(USERNAME_KEY, username);
+//   //step4-
+//   paintGreetings(username);
+// }
+
+// function paintGreetings(username) {
+//   greeting.innerText = `Hello ${username}!`;
+//   greeting.classList.remove(HIDDEN_CLASSNAME);
+// }
+
+// const savedUsername = localStorage.getItem(USERNAME_KEY);
+// if (savedUsername === null) {
+//   loginForm.classList.remove(HIDDEN_CLASSNAME);
+//   loginForm.addEventListener("submit", onLoginSubmit);
+// } else {
+//   paintGreetings(savedUsername);
+// }
+
+// =============================================================================
+// javascript-casino
+// =============================================================================
+
+const casinoForm = document.querySelector("#casino-form")
+const setNum = document.querySelector("h4 input")
+const userNum = document.querySelector("#userNum")
+const machineNum = document.querySelector("#machineNum")
+const userInputNum = document.querySelector("#userInputNum")
+const comNum = document.querySelector("#comNum")
+const result = document.querySelector("#result")
+
+function onPlay (event){
+  event.preventDefault()
+  userNum.innerText =`You chose: ${userInputNum.value},`
+  machineNum.innerText =`the machine chose: ${comNum.value}`
+}
+
+function resultText (){
+  if(userNum > comNum) {
+    result.innerText("You won!")
+  }else{
+    result.innerText("You lost!")
   }
 }
-window.addEventListener("resize", handleWindowSize)
+
+casinoForm.addEventListener("submit", onPlay)
